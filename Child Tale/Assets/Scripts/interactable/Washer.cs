171aebe9754752e1bf60212b634fa57fa9dbc5ea
hilailu <hilailu37@gmail.com>
2021-04-15 21:12:24 +0300

    Washer interactivity
    Washer door open/close + animation and sounds
    (kind of a checkpoint for me to backup if i f.ck up the savesystem)

---
+++ Child Tale/Assets/Scripts/interactable/Washer.cs
@@ -0,0 +1,44 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Washer : MonoBehaviour, IInteractable
+{
+    private Animator animator;
+    private AudioSource audioSource;
+    [SerializeField] AudioClip open;
+    [SerializeField] AudioClip close;
+    private bool isOpen = false;
+
+    private PhotonView photonView;
+
+
+    private void Start()
+    {
+        isOpen = false;
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        photonView = GetComponent<PhotonView>();
+    }
+
+
+    public void Active()
+    {
+        isOpen = !isOpen;
+        if (PhotonNetwork.OfflineMode)
+            playAnim(isOpen);
+        else
+            photonView.RPC("playAnim", RpcTarget.All, isOpen);
+    }
+
+
+    [PunRPC]
+    void playAnim(bool bol)
+    {
+        isOpen = bol;
+        animator.SetBool("IsOpened", bol);
+        if (bol)
+            audioSource.PlayOneShot(open);
+        else
+            audioSource.PlayOneShot(close);
+    }
+}
